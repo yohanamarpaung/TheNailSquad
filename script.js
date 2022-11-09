eee311863aea2e0939ef8d7ecac4bc3ba28f441d
yoohhhaaaannnnnaaaaaa <yohanavanesa7@gmail.com>
2022-11-09 23:50:14 +0700

    web the nail squad

---
+++ script.js
@@ -0,0 +1,14 @@
+function focusNav(elementTag) {
+
+    let navigasi= document.querySelectorAll(".nav-link")
+ 
+    for (const nav of navigasi) {
+     nav.classList.remove('aktif');
+    }
+ 
+    elementTag.classList.add("aktif");
+ }
+
+$(document).ready(()=>{
+    
+})
